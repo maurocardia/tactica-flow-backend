19cdd6b15fd1e885c7beea9b91e10cbc556bbbeb
maurocardia <106636288+maurocardia@users.noreply.github.com>
2026-08-31 18:21:54 -0500

    chore: remove temporary debug scripts

--- update_erick.ts
+++
@@ -1,7 +0,0 @@
-import { db } from './src/config/db.js';
-async function run() {
-  await db.query("UPDATE bot_contacts SET bot_enabled = true WHERE jid = '114156103540945@lid'");
-  console.log("Updated Erick's lid to true");
-  process.exit(0);
-}
-run().catch(console.error);
